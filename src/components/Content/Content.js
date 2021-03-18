4d2ca469fb2d52a3be2deb1349839826121068a2
IamContrerasA <icontrerasa@unsa.edu.pe>
2021-03-18 15:29:59 -0500

    default content and right sidebar

---
+++ src/components/Content/Content.js
@@ -0,0 +1,7 @@
+export const Content = () => {
+  return ( 
+    <div className = 'content'>
+      <h1>Recentyle Used</h1>
+    </div>
+  );
+}
---
+++ src/components/SideBar/Right/SidebarRight.js
@@ -0,0 +1,7 @@
+export const SidebarRight = () => {
+  return ( 
+    <div className = 'sidebar-right'>
+      <h1>Right sidebar</h1>
+    </div>
+  );
+}
