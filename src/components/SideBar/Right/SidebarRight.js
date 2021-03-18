1ddf0a3dfad15b54ab5b5f53220e93461eaf1523
IamContrerasA <icontrerasa@unsa.edu.pe>
2021-03-18 18:17:59 -0500

    v1.0 of right sidebar

--- src/components/SideBar/Right/SidebarRight.js
+++ src/components/SideBar/Right/SidebarRight.js
@@ -1,7 +1,56 @@
+import { Button } from "../../StyleComponents/button.js";
+import { Gradient } from "../../StyleComponents/gradient.js";
+import { DetailsStorageStyle } from "../../StyleComponents/storageDetails.js";
+import "./style.css";
+
 export const SidebarRight = () => {
   return ( 
     <div className = 'sidebar-right'>
-      <h1>Right sidebar</h1>
+      
+      <div className = 'user'>
+        <div className = 'graySquareContainer'>
+          <span className="graySquare"></span>
+          <span className="graySquare"></span>
+        </div>
+        <div className = 'userInfo'>
+          <span className = 'userName'>Name</span>
+          <span className = 'userPhoto'></span>
+        </div>
+      </div>
+      
+      <div className = 'lineRigth'>
+      </div>
+
+      <div className = 'storageChart'>
+        <h2>Storage</h2>
+        <svg viewBox="0 0 36 36" className="circular-chart">
+          <path className="circle"
+            values='asd'
+            strokeDasharray="80, 100"
+            d="M18 2.0845
+              a 15.9155 15.9155 0 0 1 0 31.831
+              a 15.9155 15.9155 0 0 1 0 -31.831"
+          />
+          <text x="25%" y="60%" >85%</text>
+        </svg>
+        <p>420.2 GB of 500 GB used</p>
+      </div>
+      
+
+      <div className = 'storageDetails'>
+        <DetailsStorageStyle color='#FF9F00' text1='Documents' text2='720 files' value = '200 GB'/>
+        <DetailsStorageStyle color='#689FF8' text1='Documents' text2='720 files' value = '125 GB'/>
+        <DetailsStorageStyle color='#4AC29D' text1='Documents' text2='720 files' value = '75 GB'/>
+        <DetailsStorageStyle color='#BCBECA' text1='Documents' text2='720 files' value = '50 GB'/>
+      </div>
+
+      <div className = 'upgradeMessage'>
+        <Gradient></Gradient>
+        <div>Buy more space now!</div>
+        <div className="mini-text">Upgrade to cloud premium</div>
+        <div></div>
+        <Button>Upgrade Account!</Button>
+      </div>
     </div>
   );
 }
---
+++ src/components/StyleComponents/storageDetails.js
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+
+export const detailsStorage = ({ children, className }) => {
+  const Component = `div`;
+  return <Component className={className}>
+      <span className="square"></span>
+      <span className="text1"></span>
+      <span className="mini-text"></span>
+      <span className="value"></span>
+      <span className="line"></span>
+      {children}
+    </Component>;
+};
+
+export const DetailsStorageStyle = styled(detailsStorage)`
+  display: grid;
+	grid-template-areas: 
+    "square text1 . . . value"
+    "square text2 . . . value"
+    "line line line line line line";
+
+  .square {
+    width: 28px;
+    height: 28px;
+    background: ${(props) => props.color};
+    border-radius: 8px;
+    grid-area: square;
+  }
+  .text1{
+    font-size: 14px;
+    line-height: 12px;
+    color: #343951;
+    grid-area: text1;
+  }
+  .text1::after {content: '${(props) => props.text1}';}
+
+  .mini-text{
+    justify-content: start;
+    color: #343951;
+    grid-area: text2;
+  }
+  .mini-text::after {content: '${(props) => props.text2}';}
+
+  .value {
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    justify-self: end;
+    width: 70px;
+    height: 28px;
+    background: #FFFFFF;
+    border-radius: 4px;
+    color: black
+    border-radius: 8px;
+    grid-area: value;
+  }
+  .value::after {content: '${(props) => props.value}';}
+
+  .line{
+    border: 1px solid #E0E7F1;
+    grid-area: line;
+  }
+`;
