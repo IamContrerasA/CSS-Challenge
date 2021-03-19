e4200f576cf443917eff860dd98a44becbd1c607
IamContrerasA <icontrerasa@unsa.edu.pe>
2021-03-19 17:06:52 -0500

    resize the gradient in left sidebar

--- src/components/StyleComponents/gradient.js
+++ src/components/StyleComponents/gradient.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 // If there is no props, gradient will be from sidebar-left, otherwise will be sidebar-right
 export const Gradient = styled.div`
-  width: ${props => props.component === 'left' ? "71" : "180"}px;
-  height: ${props => props.component === 'left' ? "21" : "100"}px;
+  width: ${props => props.component === 'left' ? "98" : "180"}px;
+  height: ${props => props.component === 'left' ? "25" : "100"}px;
   margin-left: 18%;
   margin-right: 18%;
   background: linear-gradient(270deg, #689EF8 0%, #FF6860 56.25%, #FF9F00 100%);
